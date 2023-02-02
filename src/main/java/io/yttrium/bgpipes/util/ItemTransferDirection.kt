a7620f8a1db42815196c8faf4d22338203c4ea22
Hannah Brooke <hannah@mail.yttrium.io>
2023-02-01 19:54:46 -0500

    begin adding item directionality to nodes

---
+++ src/main/java/io/yttrium/bgpipes/util/ItemTransferDirection.kt
@@ -0,0 +1,15 @@
+/*
+ * @author "Hannah Brooke <hannah@mail.yttrium.io>" a.k.a hotel, HotelCalifornia, hotel_california
+ *
+ * Copyright (c) 2023.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package io.yttrium.bgpipes.util
+
+enum class ItemTransferDirection {
+    Push, Pull
+}
